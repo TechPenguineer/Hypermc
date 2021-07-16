cc4c6cc047bb20e035a3c4e5086247ea554c74b5
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-16 00:02:28 -0400

    Change window title

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,10 +29,17 @@ namespace Hypermc
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(870, 438);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
