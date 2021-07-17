4a135247a0535e409a652eb46c0ad6f2a637d775
TheBoxyBear <joujouboxgaming@gmail.com>
2021-07-17 14:42:45 -0400

    Partially implement IList

---
+++ source/Controls/ModDownloadList.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace Hypermc.Controls
+{
+    partial class ModDownloadList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.table = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // table
+            // 
+            this.table.ColumnCount = 1;
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.table.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.table.Location = new System.Drawing.Point(0, 0);
+            this.table.Name = "table";
+            this.table.RowCount = 1;
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.table.Size = new System.Drawing.Size(705, 489);
+            this.table.TabIndex = 0;
+            // 
+            // ModDownloadList
+            // 
+            this.Controls.Add(this.table);
+            this.Name = "ModDownloadList";
+            this.Size = new System.Drawing.Size(705, 489);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel table;
+    }
+}
