d47f65bdc0ae04c4ca7ed12c2bfb4c74919bb7d4
chociii <chociii@gmx.ch>
2021-07-20 20:25:44 +0200

    > Added new Project regarding UI, based on WPF. > Made a rough mockup of what it could look like

---
+++ HyperMC.WpfUi.ClientApp/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HyperMC.WpfUi.ClientApp
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // enable blur for window
+            this.Loaded += MainWindow_Loaded_EnableBlur;
+        }
+
+
+        #region Window Blur
+        private void MainWindow_Loaded_EnableBlur(object sender, RoutedEventArgs e)
+        {
+			EnableBlur();
+        }
+
+        [DllImport("user32.dll")]
+		internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+
+		internal void EnableBlur()
+		{
+			var windowHelper = new WindowInteropHelper(this);
+
+			var accent = new AccentPolicy();
+			var accentStructSize = Marshal.SizeOf(accent);
+			accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
+
+			var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+			Marshal.StructureToPtr(accent, accentPtr, false);
+
+			var data = new WindowCompositionAttributeData();
+			data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
+			data.SizeOfData = accentStructSize;
+			data.Data = accentPtr;
+
+			SetWindowCompositionAttribute(windowHelper.Handle, ref data);
+
+			Marshal.FreeHGlobal(accentPtr);
+		}
+        #endregion
+
+        private void title_bar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+			if (e.ChangedButton == MouseButton.Left)
+				this.DragMove();
+        }
+    }
+
+    #region Window Composition Attribute
+
+    [StructLayout(LayoutKind.Sequential)]
+	internal struct WindowCompositionAttributeData
+	{
+		public WindowCompositionAttribute Attribute;
+		public IntPtr Data;
+		public int SizeOfData;
+	}
+
+	internal enum WindowCompositionAttribute
+	{
+		// ...
+		WCA_ACCENT_POLICY = 19
+		// ...
+	}
+
+	internal enum AccentState
+	{
+		ACCENT_DISABLED = 0,
+		ACCENT_ENABLE_GRADIENT = 1,
+		ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+		ACCENT_ENABLE_BLURBEHIND = 3,
+		ACCENT_INVALID_STATE = 4
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	internal struct AccentPolicy
+	{
+		public AccentState AccentState;
+		public int AccentFlags;
+		public int GradientColor;
+		public int AnimationId;
+	}
+
+	#endregion
+}
