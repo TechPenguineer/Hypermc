40b54e15e962dd8f3bac390f9323d54a569d8186
Matthew Stewart <60456321+mstew20@users.noreply.github.com>
2021-07-21 17:21:25 -0400

    Project cleanup (#1)
    * moved the Hypermc project into the src folder
    
    * Added all projects to the sln file
    
    * Delete HyperMC.sln
    
    removing extra sln
    
    * updated namespaces to corelate to the correct project

--- src/HyperMC/Mod.cs
+++ src/HyperMC/Mod.cs
@@ -1,7 +1,19 @@
-﻿namespace Hypermc
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hypermc;
+
+namespace Hypermc
 {
-    public class Mod
-    {
-        public string Name { get; init; }
-    }
+	public class Mod
+	{
+		public string Name { get; }
+
+		public Mod(string name)
+		{
+			Name = name;
+		}
+	}
 }
--- src/HyperMC_old/ModDisplay.cs
+++ src/HyperMC_old/ModDisplay.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace HyperMC
+namespace HyperMC_old
 {
     public partial class ModDisplay : UserControl
     {
