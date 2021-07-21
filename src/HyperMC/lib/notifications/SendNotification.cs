c30fc2b9faaf209972e3f278197dd64373578f82
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-21 18:40:04 -0400

    create notification void with args

---
+++ src/HyperMC/lib/notifications/SendNotification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace HyperMCLIB
+{
+    public class SendNotification
+    {
+        public static void buildNotification()
+        {
+            
+                
+
+
+        }
+    }
+}
