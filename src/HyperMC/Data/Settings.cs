4cacf4b695d6637f8b94894be1f381abf13593da
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-22 14:09:11 -0400

    create setting functions

--- src/HyperMC/Data/Settings.cs
+++ src/HyperMC/Data/Settings.cs
@@ -8,17 +8,34 @@ namespace HyperMcSettings
     {
         string path = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\.hypermc\\settings";
 
-        public void createSettingsFolder(string SettingFileName)
+        public void createSettingsFolder(string SettingType)
         {
-            string setting_file_path = $"{path}\\{SettingFileName}";
+            string setting_file_path = $"{path}\\{SettingType}";
             if(!Directory.Exists(setting_file_path))
             {
                 Directory.CreateDirectory(setting_file_path);
             }
         }
-        public void addSetting(string settingName, string settingValue)
+        public void addSetting(string settingFolder, string settingName, string settingValue, bool overwriteIfExists)
         {
+            void checkIfExists()
+            {
+                if (!Directory.Exists($"{path}\\{settingFolder}"))
+                {
+                    createSettingsFolder(settingFolder);
+                }
+            }
+            void writeSettingFile()
+            {
+                if(File.Exists($"{path}\\{settingFolder}\\{settingName}") && overwriteIfExists)
+                {
+                    File.Create($"{path}\\{settingFolder}\\{settingName}.json");
+                }
+            }
+            checkIfExists();
 
+
+            
         }
     }
 }
