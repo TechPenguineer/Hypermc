478f0edd4ff04f9d9bf21b526d93ddcf9b29e37e
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-22 00:04:58 -0400

    Create VersionData.cs

---
+++ src/HyperMC/Data/VersionData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ForgedCurse;
+
+
+
+
+
+
+namespace HypermcData
+{
+    public class VersionData
+    {
+        
+        ForgeClient client = new ForgeClient();
+
+        public void getForgeVersions()
+        {
+        }
+        public void getMinecraftVersions()
+        {
+            
+            client.Minecraft.RetrieveGameVersions();
+        }
+    };
+}
