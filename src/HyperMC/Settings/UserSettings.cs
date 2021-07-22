94eacd26476455933a601b8d2066f774033c88ae
Matthew Stewart <catcherdbacks@msn.com>
2021-07-22 14:15:29 -0400

    added todo item in the initialize of the settings

--- src/HyperMC/Settings/UserSettings.cs
+++ src/HyperMC/Settings/UserSettings.cs
@@ -11,6 +11,7 @@ namespace Hypermc.Settings
     {
         public string MinecraftPath { get; set; } = $@"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\.minecraft";
 
+        // TODO: intialize settings by getting the users saved settings for this application
         public void Initialize()
         {
             
