8084030073f121090bcaf06ac0e485f8548640ad
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-25 01:17:23 -0400

    Update SendNotification.cs

--- src/HyperMC/lib/notifications/SendNotification.cs
+++ src/HyperMC/lib/notifications/SendNotification.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Toolkit.Uwp.Notifications;
-
+using DesktopToast;
+using DesktopNotifications;
 namespace HyperMCLIB
 {
     public class SendNotification
     {
-        public static void buildNotification()
+        public void send(String title, String body)
         {
+            ToastContentBuilder content = new ToastContentBuilder()
+                 .AddText(title)
+                 .AddText(body);
         }
     }
 }
