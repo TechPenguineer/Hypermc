a673b3b03f06f783ef5284a8435b89ceae513b1a
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-05 18:40:36 -0400

    removed old version of hypermc

--- cli/src/hmc.c
+++ cli/src/hmc.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     if(strcmp(argv[1], "config")==0 && strcmp(argv[2], "-s")==0)
     {
         if(argc != 4){
-            
+            printf("Invalid Argumentation");
         }
     }
 }
