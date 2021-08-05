1ced5312efd4b3e8f6b2a2d7b0941d8689449c77
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-05 18:39:14 -0400

    use new argument parser

--- cli/src/hmc.c
+++ cli/src/hmc.c
@@ -1,6 +1,14 @@
 #include "includer.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(int argv, char *argc[])
+int main(int argc, char *argv[])
 {
-    
+    if(strcmp(argv[1], "config")==0 && strcmp(argv[2], "-s")==0)
+    {
+        if(argc != 4){
+            
+        }
+    }
 }
